c50e32a5c537aac955874444acf532abbd13785e
koderscript99-collab <koderscript99@gmail.com>
2026-07-02 17:44:26 +0100

    Add files via upload
    2nd commit

--- forex/settings.py
+++ forex/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^-w3!$#ag(_jv=^!dz1zqf&r%74x1_&0hz***i$qt9hq3++ank
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
